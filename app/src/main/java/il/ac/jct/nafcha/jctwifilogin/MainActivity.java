a3ace392538917183952707ecaf8679ef83c8ab5
Haim Nafcha <nafhah@gmail.com>
2015-04-30 13:23:40 +0300

    Language selection, and more fixes

--- app/src/main/java/il/ac/jct/nafcha/jctwifilogin/MainActivity.java
+++ app/src/main/java/il/ac/jct/nafcha/jctwifilogin/MainActivity.java
@@ -2,14 +2,21 @@ package il.ac.jct.nafcha.jctwifilogin;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +25,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //set the language
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        String language = sharedPref.getString("language", "");
+        if (language != "default") {
+            Locale myLocale = new Locale(language);
+            Resources res = getResources();
+            DisplayMetrics dm = res.getDisplayMetrics();
+            Configuration conf = res.getConfiguration();
+            conf.locale = myLocale;
+            res.updateConfiguration(conf, dm);
+        }
+
         setContentView(R.layout.activity_main);
 
         final SharedPreferences prefs = new ObscuredSharedPreferences(
@@ -47,6 +66,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -68,6 +89,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(this, SettingsActivity.class));
                 return true;
             case R.id.action_about:
+                startActivity(new Intent(this, About.class));
                 return true;
         }
 

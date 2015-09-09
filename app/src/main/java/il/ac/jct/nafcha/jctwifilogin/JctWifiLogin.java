630a9ee15ac7d5d7579d83613ae8a957631acc4d
Haim Nafcha <nafhah@gmail.com>
2015-09-09 23:30:34 +0300

    Clean unused imports

--- app/src/main/java/il/ac/jct/nafcha/jctwifilogin/JctWifiLogin.java
+++ app/src/main/java/il/ac/jct/nafcha/jctwifilogin/JctWifiLogin.java
@@ -4,7 +4,6 @@ import android.annotation.TargetApi;
 import android.app.IntentService;
 import android.app.Notification;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -15,26 +14,14 @@ import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -111,14 +98,6 @@ public class JctWifiLogin extends IntentService{
                 reportStateChange();
             }
 
-//            try {
-//                synchronized (this) {
-//                    this.wait(3000);
-//                }
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-
         } catch (IOException e) {
             // Log exception
             e.printStackTrace();
@@ -158,13 +137,10 @@ public class JctWifiLogin extends IntentService{
     }
 
     private void updateOngoingNotification(boolean notify) {
-//        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, new Intent() , 0);
         mNotification = new NotificationCompat.Builder(this).setContentTitle(getString(R.string.notify_login_ongoing_title))
                 .setContentText(getString(R.string.notify_request_wifi_ongoing_text))
                 .setSmallIcon(R.drawable.ic_wifi_black_24dp)
                 .build();
- //       mNotification.setLatestEventInfo(this, getString(R.string.notify_login_ongoing_title), message, contentIntent);
-
         if (notify) {
             mNotifMan.notify(LOGIN_ONGOING_ID, mNotification);
         }
--- app/src/main/java/il/ac/jct/nafcha/jctwifilogin/NetworkStateChanged.java
+++ app/src/main/java/il/ac/jct/nafcha/jctwifilogin/NetworkStateChanged.java
@@ -1,33 +1,13 @@
 package il.ac.jct.nafcha.jctwifilogin;
 
-import android.annotation.TargetApi;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.ConnectivityManager;
-import android.net.Network;
 import android.net.NetworkInfo;
 import android.net.wifi.WifiManager;
-import android.os.Build;
 import android.preference.PreferenceManager;
 import android.util.Log;
-import android.net.wifi.WifiManager;
-import android.widget.Toast;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class NetworkStateChanged extends BroadcastReceiver {
 
